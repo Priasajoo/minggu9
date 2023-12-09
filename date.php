fd3bf72d3795830cd322586a906c6fd1bab09d40
priasajoo <zainulrosikin981@gmail.com>
2023-12-09 09:23:03 +0700

    task2

--- date.php
+++ date.php
@@ -1,4 +1,22 @@
 <?php
+$todayDate = "9 des 2023";
+$returnDate = date('d-m-Y , strtotime(+100 day($todayDate)));
 
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-  echo date("1");
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head?
+
+  <body>
+       <h3>
+           Tanggal hari ini = <?= $todayDate; ?>
+           <br>
+           Tanggal 100 hari dari skrg = <?= $returnDate; ?>
+          </h3>
+        </body>
+
+        </html>

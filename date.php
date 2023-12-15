6f47b5c4804352e45a73c9188ac10290c3b8f634
priasajoo <zainulrosikin981@gmail.com>
2023-12-15 07:19:01 +0700

    task5

--- date.php
+++ date.php
@@ -1,11 +1,11 @@
 <?php
-function tambahHari($hari, $tanggal) {
-    $waktu = strtotime("+$hari days", strtotime($tanggal));
-    return date('d F Y', $waktu);
-}
+$day = 18;
+$month = 10;
+$year = 2003
 
-$hariIni = "9 Desember 2023";
-$jumlahHari = -500;
-$hasil = tambahHari($jumlahHari, $hariIni);
-echo "500 hari yang lalu dari tanggal $hariIni adalah $hasil";
+$timestamp = mktime(0, 0, 0, $month, $day, $year);
+
+$harilahir = date("L", $timestamp);
+
+echo "Hari lahir saya adalah: " . $harilahir;
 ?>
